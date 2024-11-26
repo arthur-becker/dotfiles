90db41ce40e9915736f00f668f8ec23389d6381b
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-11-26 11:08:16 -0500

    installation of catppuccin also with brew

--- dotfiles-brew-install.sh
+++ dotfiles-brew-install.sh
@@ -2,3 +2,6 @@ brew install starship
 brew install zsh-autosuggestions
 brew install zoxide
 brew install tmux
+
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.1 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
